c988a8d55a85eaa8b64197409a11d0b369ad6e26
ltenriol <150988189+ltenriol@users.noreply.github.com>
2025-07-24 19:25:48 -0400

    part1 test

--- src/part1-etl.py
+++ src/part1-etl.py
@@ -8,7 +8,7 @@ pred_universe_raw = pd.read_csv('https://www.dropbox.com/scl/fi/69syqjo6pfrt9123
 arrest_events_raw = pd.read_csv('https://www.dropbox.com/scl/fi/wv9kthwbj4ahzli3edrd7/arrest_events_lab6.feather?rlkey=mhxozpazqjgmo6qqahc2vd0xp&dl=1')
 pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw.filing_date)
 arrest_events_raw['arrest_date_event'] = pd.to_datetime(arrest_events_raw.filing_date)
-pred_universe_raw.drop(columns=['filing_date'], inplace=True)
-arrest_events_raw.drop(columns=['filing_date'], inplace=True)
 
 # Save both data frames to `data/` -> 'pred_universe_raw.csv', 'arrest_events_raw.csv'
+pred_universe_raw.to_csv("data/pred_universe_raw.csv", index=False)
+arrest_events_raw.to_csv("data/arrest_events_raw.csv", index=False)
